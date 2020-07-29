42d2c80a34a1f0cd36b1b5bcbae24326aa1ec556
于良杰 <920575414@qq.com>
2020-07-29 19:53:58 +0800

    DateUtil

--- Code/util/DateUtil.java
+++ Code/util/DateUtil.java
@@ -5,23 +5,41 @@ import java.util.Date;
 public class DateUtil{
 
 	/**
+	* 获取日期所在月的第一天
+	*/
+	public static Date getMonthStart(Date date) {
+        Calendar nowDateCal = Calendar.getInstance();
+        nowDateCal.setTime(date);
+
+        //设置为当月第一天
+        nowDateCal.set(Calendar.DAY_OF_MONTH, 1);
+        //将小时至0
+        nowDateCal.set(Calendar.HOUR_OF_DAY, 0);
+        //将秒至0
+        nowDateCal.set(Calendar.SECOND, 0);
+        //将分钟至0
+        nowDateCal.set(Calendar.MINUTE, 0);
+        return nowDateCal.getTime();
+    }
+
+	/**
 	* 获取日期所在月的最后一天
 	*/
 	public static Date getMonthEnd(Date date){
-        Calendar c = Calendar.getInstance();
-        c.setTime(date);
+        Calendar nowDateCal = Calendar.getInstance();
+        nowDateCal.setTime(date);
 
         //设置为当月最后一天
-        c.set(Calendar.DAY_OF_MONTH, c.getActualMaximum(Calendar.DAY_OF_MONTH));
+        nowDateCal.set(Calendar.DAY_OF_MONTH, nowDateCal.getActualMaximum(Calendar.DAY_OF_MONTH));
         //将小时至23
-        c.set(Calendar.HOUR_OF_DAY, 23);
+        nowDateCal.set(Calendar.HOUR_OF_DAY, 23);
         //将分钟至59
-        c.set(Calendar.MINUTE, 59);
+        nowDateCal.set(Calendar.MINUTE, 59);
         //将秒至59
-        c.set(Calendar.SECOND,59);
+        nowDateCal.set(Calendar.SECOND,59);
 
         // 获取本月最后一天的时间
-        return c.getTime();
+        return nowDateCal.getTime();
     }
 
 }
